10537373b7dda3216bcc139cf3ecd99ddebe4592
timer4ik <timergalievadel@yandex.ru>
2023-04-02 13:41:03 +0300

    !add

---
+++ src/routes/promo-router.js
@@ -0,0 +1,13 @@
+const CategoryController = require("../controllers/category-controller")
+
+const Router = require("express").Router
+
+const router = Router()
+
+router.get("/categories", CategoryController.getCategories)
+router.get("/categories/:id", CategoryController.getCategoryById)
+router.post("/categories", CategoryController.createCategory)
+router.put("/categories/:id", CategoryController.updateCategory)
+router.delete("/categories/:id", CategoryController.getCategoryById)
+
+module.exports = router
