9ccc58a1297df05d50c2a395b039ba8c19a7a485
adel <timergalievadel@yandex.ru>
2023-07-07 09:27:00 +0300

    !fix

--- src/routes/review-router.js
+++ src/routes/review-router.js
@@ -1,12 +1,13 @@
 const ReviewController = require("../controllers/review-controller")
+const authMiddleware = require("../middlewares/auth-middleware")
 
 const Router = require("express").Router
 
 const router = Router()
 
-router.get("/review", ReviewController.getReviews)
+router.get("/reviews", ReviewController.getReviews)
 // router.get("/review/:id", ReviewController.getPropertyById)
-router.post("/review", ReviewController.createReview)
+router.post("/reviews",authMiddleware, ReviewController.createReview)
 // router.put("/review/:id", ReviewController.updateProperty)
 // router.delete("/review/:id", ReviewController.deleteProperty)
 
